b68c7be8f85df1d5c887479b7bd09aca6b81a43c
apoorvnema <apoorvnema38@gmail.com>
2023-12-08 14:12:05 +0530

    v1.2

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ app.post("/",function(req,res){
     };
     const request = https.request(url, options, function(response){
         if(response.statusCode == 200){
-            res.sendFile(path. join(__dirname, 'public', 'success.html'));
+            res.sendFile(path.join(__dirname, 'public', 'success.html'));
         }
         else{
-            res.sendFile(path. join(__dirname, 'public', 'failure.html'));
+            res.sendFile(path.join(__dirname, 'public', 'failure.html'));
         }
 
         response.on("data",function(data){
